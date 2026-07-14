7eb67f8371f78af4bef15a45dd2526abcc644822
Siranjeevan <smartjeevith2k@gmail.com>
2026-07-14 23:33:16 +0530

    Initial Commit

---
+++ Pinbox/PinboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinboxApp.swift
+//  Pinbox
+//
+//  Created by Jeevith on 7/14/26.
+//
+
+import SwiftUI
+
+@main
+struct PinboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
