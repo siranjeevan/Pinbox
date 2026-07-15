a6b219176896682079c1ca114ca6a14240d3507b
Siranjeevan <smartjeevith2k@gmail.com>
2026-07-15 06:28:15 +0530

    ZeroDrop(06:28 AM): Window Closure Save

--- Pinbox/App/AppDelegate.swift
+++ Pinbox/App/AppDelegate.swift
@@ -2,22 +2,62 @@ import SwiftUI
 import AppKit
 import SwiftData
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSPopoverDelegate {
+    var statusItem: NSStatusItem?
+    var popover: NSPopover?
+    var eventMonitor: Any?
     
     func applicationDidFinishLaunching(_ notification: Notification) {
         print("🚀 Pinbox applicationDidFinishLaunching called!")
         
-        // Request accessibility permissions for auto-paste if needed
         checkAccessibilityPermissions()
-        
-        // Hide dock icon
         NSApp.setActivationPolicy(.accessory)
         
-        // Start clipboard monitoring
+        // Create Popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 420, height: 560)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: PopoverContentView().modelContainer(PinboxSchema.container))
+        self.popover = popover
+        
+        // Create Status Item
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let button = statusItem?.button {
+            if let image = NSImage(systemSymbolName: "doc.on.clipboard", accessibilityDescription: "Pinbox") {
+                image.isTemplate = true
+                button.image = image
+            } else {
+                button.title = "Pinbox"
+            }
+            button.action = #selector(togglePopover)
+            button.target = self
+        }
+        
         ClipboardService.shared.startMonitoring()
         
-        // Start Hotkey Service for global shortcuts
+        HotkeyService.shared.onToggle = { [weak self] in
+            self?.togglePopover()
+        }
         HotkeyService.shared.start()
+        
+        eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+            if let popover = self?.popover, popover.isShown {
+                popover.performClose(event)
+            }
+        }
+    }
+    
+    @objc func togglePopover() {
+        guard let button = statusItem?.button, let popover = popover else { return }
+        
+        if popover.isShown {
+            popover.performClose(nil)
+        } else {
+            NSApp.activate(ignoringOtherApps: true)
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            popover.contentViewController?.view.window?.makeKey()
+        }
     }
     
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
